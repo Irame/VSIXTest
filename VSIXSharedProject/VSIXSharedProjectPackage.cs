7a22aef191f86665b5f5cba816c6d86834ea4b42
Irame <irame94@gmail.com>
2021-12-30 23:44:03 +0100

    Added ToolWindow to dummy project

--- VSIXSharedProject/VSIXSharedProjectPackage.cs
+++ VSIXSharedProject/VSIXSharedProjectPackage.cs
@@ -25,6 +25,8 @@ namespace VSIXSharedProject
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(VSIXSharedProjectPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideToolWindow(typeof(ToolWindow))]
     public sealed class VSIXSharedProjectPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +48,7 @@ namespace VSIXSharedProject
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await ToolWindowCommand.InitializeAsync(this);
         }
 
         #endregion
